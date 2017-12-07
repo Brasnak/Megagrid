04bcd541857d2fe8ca34695ab951650fd8e2deb1
Brasnak <brasnak@hotmail.com>
2017-12-07 22:51:53 +0100

    fully functional screen scaling

--- main.lua
+++ main.lua
@@ -1,31 +1,43 @@
 function love.load(arg)
   token = love.graphics.newImage("sprites/token.png")
+  gamegrid = love.graphics.newSpriteBatch(love.graphics.newImage("sprites/Field.png"), 100)
+  field = love.graphics.newQuad(0, 0, 64, 64, 256, 64)
   love.graphics.setDefaultFilter("linear", "nearest", 1)
   love.window.setTitle("Megagrid")
   love.window.setMode(320, 320, {resizable = true, minwidth = 320, minheight = 320})
+  scaleimage, offsetH, offsetW = 1, 0, 0 -- Use scaleimage for image scale and add offset to image placement
+  baseW, baseH = 320, 320  --Screensize at scale 1 for 5x5 sprites
+  for y = 0, 4, 1 do
+    for x = 0, 4, 1 do
+      gamegrid:add(field, x * 64, y * 64)
+    end
+  end
+end
+
+function love.update(dt)
+
+end
+
+function love.resize(w, h)
+  if w > h then
+    offsetH = 0
+    offsetW = (w - h) / 2
+    scaleimage = h / baseH
+  else
+    offsetW = 0
+    offsetH = (h - w) / 2
+    scaleimage = w / baseW
+  end
 end
 
 function love.draw()
-  scaleimage, scaleheight, scalewidth = calculateScale()
   love.graphics.clear()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(gamegrid, 0 + offsetW, 0 + offsetH, 0, scaleimage, scaleimage)
   love.graphics.setColor(255, 160, 20)
-  love.graphics.draw(token, 30 * scalewidth, 30 * scaleheight, 0 , scaleimage, scaleimage)
-end
-
---Used to determine Sprite scale and relative position
---Currently designed for an original window of 320x320
-
-function calculateScale()
-  scalewidth, scaleheight, scaleimage = 1
-  width, height = love.window.getDesktopDimensions()
-  if width > height then
-    scalewidth = width / 320
-    scaleheight, scaleimage = height / 320
-  elseif height < width then
-    scaleheight = height / 320
-    scalewidth, scaleimage = width / 320
-  else
-    scaleimage, scalewidth, scaleheight = height / 320
+  for y = 0, 4, 1 do
+    for x = 0, 4, 1 do
+      love.graphics.draw(token, x * 64 * scaleimage + offsetW, y * 64 * scaleimage + offsetH, 0 , scaleimage, scaleimage)
+    end
   end
-  return scaleimage, scaleheight, scalewidth
 end
