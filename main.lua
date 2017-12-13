17f164c6187bd47628120f5d047954f8f397068e
Brasnak <brasnak@hotmail.com>
2017-12-13 12:59:17 +0100

    Clickable Window, Improved Token Sprite

--- main.lua
+++ main.lua
@@ -1,17 +1,15 @@
 function love.load(arg)
+  math.randomseed(os.time())
   token = love.graphics.newImage("sprites/token.png")
-  gamegrid = love.graphics.newSpriteBatch(love.graphics.newImage("sprites/Field.png"), 100)
   field = love.graphics.newQuad(0, 0, 64, 64, 256, 64)
-  love.graphics.setDefaultFilter("linear", "nearest", 1)
+  love.graphics.setDefaultFilter("nearest", "nearest", 1)
   love.window.setTitle("Megagrid")
   love.window.setMode(320, 320, {resizable = true, minwidth = 320, minheight = 320})
   scaleimage, offsetH, offsetW = 1, 0, 0 -- Use scaleimage for image scale and add offset to image placement
   baseW, baseH = 320, 320  --Screensize at scale 1 for 5x5 sprites
-  for y = 0, 4, 1 do
-    for x = 0, 4, 1 do
-      gamegrid:add(field, x * 64, y * 64)
-    end
-  end
+  gridsizeX, gridsizeY = 5, 5
+  colors = {color(211, 121, 121), color(211, 167, 121), color(210, 210, 121), color(165, 211, 121), color(121, 164, 211), color(210, 121, 163) }
+  gamegrid, spritegrid = createGrid(gridsizeX, gridsizeY)
 end
 
 function love.update(dt)
@@ -33,11 +31,53 @@ end
 function love.draw()
   love.graphics.clear()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(gamegrid, 0 + offsetW, 0 + offsetH, 0, scaleimage, scaleimage)
-  love.graphics.setColor(255, 160, 20)
-  for y = 0, 4, 1 do
-    for x = 0, 4, 1 do
-      love.graphics.draw(token, x * 64 * scaleimage + offsetW, y * 64 * scaleimage + offsetH, 0 , scaleimage, scaleimage)
+  love.graphics.draw(spritegrid, 0 + offsetW, 0 + offsetH, 0, scaleimage, scaleimage)
+  for y, rowTable in pairs(gamegrid) do
+    for x, color in pairs(rowTable) do
+      if color > 0 and color <= #colors then
+        drawDrawable(token, colors[color], x, y)
+      end
     end
   end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+  if x >= offsetW and x < 64 * scaleimage * gridsizeX + offsetW and y >= offsetH and y < 64 *  scaleimage * gridsizeY + offsetH then
+    local tileX = math.floor((x - offsetW) / (64 * scaleimage))
+    local tileY = math.floor((y - offsetH) / (64 * scaleimage))
+    gamegrid[tileY][tileX] = (gamegrid[tileY][tileX] + 1) % (#colors + 1)
+  end
+end
+
+-- math.floor(math.random(1, #colors))
+------------------------------------------------------------------------------------------------------------------------------------
+
+
+--used to define colors faster
+function color(r, g, b)
+  local color = {}
+
+  color["r"] = r
+  color["g"] = g
+  color["b"] = b
+
+  return color
+end
+
+function createGrid(sizeX, sizeY)
+  local logic = {}
+  local sprites = love.graphics.newSpriteBatch(love.graphics.newImage("sprites/Field.png"), 100)
+  for y = 0, sizeY - 1, 1 do
+    logic[y] = {}
+    for x = 0, sizeX - 1, 1 do
+      sprites:add(field, x * 64, y * 64)
+      logic[y][x] = 0  --CHANGE BACK TO "= 0" LATER!!!!!
+    end
+  end
+  return logic, sprites
+end
+
+function drawDrawable(sprite, color, tileX, tileY)
+  love.graphics.setColor(color.r, color.g, color.b, 255)
+  love.graphics.draw(sprite, tileX * 64 * scaleimage + offsetW, tileY * 64 * scaleimage + offsetH, 0, scaleimage, scaleimage)
 end
