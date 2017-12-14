315b1ace90562d020514c69b09942f9b5c3eae8d
Brasnak <brasnak@hotmail.com>
2017-12-14 13:11:19 +0100

    Improves token drawing, adds gitignore

--- main.lua
+++ main.lua
@@ -3,9 +3,10 @@ function love.load(arg)
   token = love.graphics.newImage("sprites/token.png")
   field = love.graphics.newQuad(0, 0, 64, 64, 256, 64)
   cursor = love.image.newImageData("sprites/cursor.png")
-  colors = {color(211, 121, 121), color(211, 167, 121), color(210, 210, 121), color(165, 211, 121), color(121, 164, 211), color(210, 121, 163) }
+  colors = {color(211, 121, 121), color(211, 167, 121), color(210, 210, 121), color(165, 211, 121), color(121, 164, 211), color(210, 121, 163)}
   currentColor = 0
-  love.graphics.setDefaultFilter("nearest", "nearest", 1)
+  drawColor = 0
+  love.graphics.setDefaultFilter("linear", "linear", 1)
   love.window.setTitle("Megagrid")
   love.window.setMode(320, 320, {resizable = true, minwidth = 320, minheight = 320})
   scaleimage, offsetH, offsetW = 1, 0, 0 -- Use scaleimage for image scale and add offset to image placement
@@ -13,6 +14,7 @@ function love.load(arg)
   gridsizeX, gridsizeY = 5, 5
   updateCursor()
   gamegrid, spritegrid = createGrid(gridsizeX, gridsizeY)
+  love.graphics.setColor(255, 255, 255)
 end
 
 function love.update(dt)
@@ -33,22 +35,22 @@ end
 
 function love.draw()
   love.graphics.clear()
-  love.graphics.setColor(255, 255, 255)
+
   love.graphics.draw(spritegrid, 0 + offsetW, 0 + offsetH, 0, scaleimage, scaleimage)
   for y, rowTable in pairs(gamegrid) do
     for x, color in pairs(rowTable) do
       if color > 0 and color <= #colors then
-        drawDrawable(token, colors[color], x, y)
+        drawToken(color, x, y)
       end
     end
   end
 end
 
 function love.mousepressed(x, y, button, isTouch)
-  if x >= offsetW and x < 64 * scaleimage * gridsizeX + offsetW and y >= offsetH and y < 64 *  scaleimage * gridsizeY + offsetH and love.mouse.isDown(1) then
+  if x >= offsetW and x < baseW * scaleimage + offsetW and y >= offsetH and y < baseH * scaleimage + offsetH and love.mouse.isDown(1) then
     local tileX = math.floor((x - offsetW) / (64 * scaleimage))
     local tileY = math.floor((y - offsetH) / (64 * scaleimage))
-    gamegrid[tileY][tileX] = currentColor--(gamegrid[tileY][tileX] + 1) % (#colors + 1)
+    gamegrid[tileY][tileX] = currentColor
   end
 end
 
@@ -59,7 +61,7 @@ function love.wheelmoved(x, y)
 end
 
 -- math.floor(math.random(1, #colors)) | Use for correct answer generation
-------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------New Functions-----------------------------------------------
 
 
 --used to define colors faster
@@ -73,6 +75,7 @@ function color(r, g, b)
   return color
 end
 
+--creats the puzzle grid both visualy and logically
 function createGrid(sizeX, sizeY)
   local logic = {}
   local sprites = love.graphics.newSpriteBatch(love.graphics.newImage("sprites/Field.png"), 100)
@@ -86,13 +89,19 @@ function createGrid(sizeX, sizeY)
   return logic, sprites
 end
 
-function drawDrawable(sprite, color, tileX, tileY)
-  love.graphics.setColor(color.r, color.g, color.b, 255)
-  love.graphics.draw(sprite, tileX * 64 * scaleimage + offsetW, tileY * 64 * scaleimage + offsetH, 0, scaleimage, scaleimage)
+--used to draw the tokens
+function drawToken(colorID, tileX, tileY)
+  drawColor = colorID
+  local spriteData = love.image.newImageData("sprites/token.png")
+  spriteData:mapPixel(colorToken)
+  love.graphics.draw(love.graphics.newImage(spriteData), tileX * 64 * scaleimage + offsetW, tileY * 64 * scaleimage + offsetH, 0, scaleimage, scaleimage)
+
 end
 
-function colorInActive(x, y, r, g, b, a)
+--colors cursor imagedata in currently active color
+function colorCursor(x, y, r, g, b, a)
   local toColor
+
   if currentColor == 0 then
     toColor = color(255, 255, 255)
   else
@@ -106,7 +115,19 @@ function colorInActive(x, y, r, g, b, a)
   return r, g, b, a
 end
 
+function colorToken(x, y, r, g, b, a)
+  local toColor
+  toColor = colors[drawColor]
+
+  r = toColor.r * (r / 255)
+  g = toColor.g * (g / 255)
+  b = toColor.b * (b / 255)
+
+  return r, g, b, a
+end
+
+--sets the cursor to the currently selected color
 function updateCursor()
-  cursor:mapPixel(colorInActive)
+  cursor:mapPixel(colorCursor)
   love.mouse.setCursor(love.mouse.newCursor(cursor, 30, 30))
 end
